45676f1fcbe43385504ea289858d5b4134f434dd
Oleg <35769958+trudoholic@users.noreply.github.com>
2024-02-22 20:43:03 +0300

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+import {Canvas} from "@react-three/fiber"
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <div id="container">
+      <div className="aim"/>
+      <Canvas camera={{ fov: 45 }}>
+        <App />
+      </Canvas>
+    </div>
+  </React.StrictMode>,
+)
