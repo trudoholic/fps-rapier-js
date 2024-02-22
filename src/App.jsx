7122a5022bb27adf2edb6aa555d6b5926273e6cd
Oleg <35769958+trudoholic@users.noreply.github.com>
2024-02-22 22:30:35 +0300

    Ground

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import {Sky} from "@react-three/drei"
+import {Ground} from "./Ground.jsx"
 
 const App = () => {
 
   return (
     <>
+      <Sky sunPosition={[100, 20, 100]} />
+      <Ground />
     </>
   )
 }
