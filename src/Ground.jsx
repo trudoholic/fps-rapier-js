ee1c04e9e6f7e800b371954f15e593200a017836
Oleg <35769958+trudoholic@users.noreply.github.com>
2024-02-24 15:29:22 +0300

    Jump

--- src/Ground.jsx
+++ src/Ground.jsx
@@ -1,6 +1,6 @@
 import * as THREE from "three"
 import { useTexture } from "@react-three/drei"
-import {RigidBody} from "@react-three/rapier"
+import {CuboidCollider, RigidBody} from "@react-three/rapier"
 import floorTexture from "./assets/floor.png"
 
 export const Ground = () => {
@@ -8,11 +8,12 @@ export const Ground = () => {
   texture.wrapS = texture.wrapT = THREE.RepeatWrapping
 
   return (
-    <RigidBody>
+    <RigidBody type="fixed" colliders={false}>
       <mesh position={[0, -5, 0]} rotation-x={-Math.PI / 2}>
         <planeGeometry args={[500, 500]} />
         <meshStandardMaterial color="gray" map={texture} map-repeat={[100, 100]} />
       </mesh>
+      <CuboidCollider args={[500, 2, 500]} position={[0, -2, 0]}/>
     </RigidBody>
   )
 }
--- src/Player.jsx
+++ src/Player.jsx
@@ -1,7 +1,8 @@
 import * as THREE from "three"
+import * as RAPIER from "@dimforge/rapier3d-compat"
 import {useRef} from "react"
 import {useFrame} from "@react-three/fiber"
-import {RigidBody} from "@react-three/rapier"
+import {RigidBody, useRapier} from "@react-three/rapier"
 import {usePersonControls} from "./hooks.js"
 
 const MOVE_SPEED = 5
@@ -11,11 +12,13 @@ const sideVector = new THREE.Vector3()
 
 export const Player = () => {
   const playerRef = useRef()
-  const { forward, backward, left, right } = usePersonControls()//, jump
+  const { forward, backward, left, right, jump } = usePersonControls()
+  const rapier = useRapier()
 
   useFrame((/*state*/) => {
     if (!playerRef.current) return
 
+    // moving player
     const velocity = playerRef.current?.linvel()
 
     frontVector.set(0, 0, backward - forward)
@@ -24,7 +27,20 @@ export const Player = () => {
 
     playerRef.current?.wakeUp()
     playerRef.current?.setLinvel({ x: direction.x, y: velocity.y, z: direction.z })
-  });
+
+    // jumping
+    const world = rapier.world
+    const ray = world.castRay(
+      new RAPIER.Ray(playerRef.current?.translation(), { x: 0, y: -1, z: 0 }), 4.0, true
+    )
+    const grounded = ray && ray.collider && Math.abs(ray.toi) <= 1
+
+    if (jump && grounded) doJump()
+  })
+
+  const doJump = () => {
+    playerRef.current?.setLinvel({x: 0, y: 8, z: 0});
+  }
 
   return (
     <>
